5b3d80ecd8d381c712d6e59dc4b4d92bda185fd6
Sebastian <55423389+Sebastian-RP@users.noreply.github.com>
2024-01-08 18:15:27 -0500

    create VillaController - connected to database

---
+++ MagicVilla_API/Migrations/20240108230656_AlimentarTablaVilla-metroscuadrados.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVillaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlimentarTablaVillametroscuadrados : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MetroCuadrados",
+                table: "Villas",
+                newName: "MetrosCuadrados");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "FechaActualizacion", "FechaCreacion" },
+                values: new object[] { new DateTime(2024, 1, 8, 18, 6, 56, 308, DateTimeKind.Local).AddTicks(5585), new DateTime(2024, 1, 8, 18, 6, 56, 308, DateTimeKind.Local).AddTicks(5572) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "FechaActualizacion", "FechaCreacion" },
+                values: new object[] { new DateTime(2024, 1, 8, 18, 6, 56, 308, DateTimeKind.Local).AddTicks(5589), new DateTime(2024, 1, 8, 18, 6, 56, 308, DateTimeKind.Local).AddTicks(5588) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MetrosCuadrados",
+                table: "Villas",
+                newName: "MetroCuadrados");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "FechaActualizacion", "FechaCreacion" },
+                values: new object[] { new DateTime(2024, 1, 8, 17, 28, 5, 770, DateTimeKind.Local).AddTicks(7603), new DateTime(2024, 1, 8, 17, 28, 5, 770, DateTimeKind.Local).AddTicks(7590) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "FechaActualizacion", "FechaCreacion" },
+                values: new object[] { new DateTime(2024, 1, 8, 17, 28, 5, 770, DateTimeKind.Local).AddTicks(7607), new DateTime(2024, 1, 8, 17, 28, 5, 770, DateTimeKind.Local).AddTicks(7606) });
+        }
+    }
+}
--- MagicVilla_API/Program.cs
+++ MagicVilla_API/Program.cs
@@ -1,11 +1,24 @@
+using MagicVilla_API.Datos;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//configuramos el DbContext, indicando el tipo de base de datos a usar y su cadena de conexion
+//luego se usara en el controlador por medio de inyeccion de dependencias
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
